7a301d65eebca9b6917431df5dafe8ca333c7594
Ken Overholt <kroverholt@gmail.com>
2017-09-03 16:49:48 -0400

    Update ProcessMeasurement from EKF

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -11,6 +11,11 @@ using std::vector;
  * Initializes Unscented Kalman filter
  */
 UKF::UKF() {
+  
+  is_initialized_ = false;
+
+  previous_timestamp_ = 0;
+  
   // if this is false, laser measurements will be ignored (except during init)
   use_laser_ = true;
 
@@ -59,13 +64,110 @@ UKF::~UKF() {}
  * @param {MeasurementPackage} meas_package The latest measurement data of
  * either radar or laser.
  */
-void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
-  /**
-  TODO:
+void UKF::ProcessMeasurement(MeasurementPackage measurement_pack) {
+  
+  //KRO2: This entire method is from EKF so update/remove as necessary
+  
+  /*****************************************************************************
+   *  Initialization
+   ****************************************************************************/
+  if (!is_initialized_) {
+    /**
+      * Initialize the state x_ with the first measurement.
+      * Create the covariance matrix.
+    */
+    // first measurement
+    x_ = VectorXd(4);
+    x_ << 1, 1, 0, 0;  //KRO important for RMSE; first two will be overwritten but I should play with the last 2
 
-  Complete this function! Make sure you switch between lidar and radar
-  measurements.
-  */
+    if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
+      /**
+      Convert radar from polar to cartesian coordinates and initialize state.
+      */
+      //measurement_pack.raw_measurements_(0) is rho
+      //measurement_pack.raw_measurements_(1) is phi
+      x_(0) = measurement_pack.raw_measurements_(0) * cos(measurement_pack.raw_measurements_(1)); 
+      x_(1) = measurement_pack.raw_measurements_(0) * sin(measurement_pack.raw_measurements_(1));
+    }
+    else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
+      /**
+      Initialize state.
+      */
+      //measurement_pack.raw_measurements_(0) is x
+      //measurement_pack.raw_measurements_(1) is y
+
+      x_(0) = measurement_pack.raw_measurements_(0);
+      x_(1) = measurement_pack.raw_measurements_(1);
+    }
+
+    F_ << 1, 0, 0, 0,  //KRO 1 diagonal matrix
+          0, 1, 0, 0,
+          0, 0, 1, 0,
+          0, 0, 0, 1;
+    
+    previous_timestamp_ = measurement_pack.timestamp_;
+    
+    // done initializing, no need to predict or update
+    is_initialized_ = true;
+    return;
+  }
+
+  /*****************************************************************************
+   *  Prediction
+   ****************************************************************************/
+
+  /**
+     * Update the state transition matrix F according to the new elapsed time.
+      - Time is measured in seconds.
+     * Update the process noise covariance matrix.
+     * Use noise_ax = 9 and noise_ay = 9 for the Q matrix.
+   */
+
+  /*
+  float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0; //dt expressed in seconds
+  previous_timestamp_ = measurement_pack.timestamp_;
+  
+  float dt_2 = dt * dt;
+  float dt_3 = dt_2 * dt;
+  float dt_4 = dt_3 * dt;
+  
+  //Modify the F matrix so that the time is integrated  L5, S8
+  ekf_.F_(0, 2) = dt;
+  ekf_.F_(1, 3) = dt;
+  
+  // set the process covariance matrix Q as in L5, s9
+  ekf_.Q_ = MatrixXd(4, 4);
+  ekf_.Q_ << noise_ax_*(dt_4/4), 0, noise_ax_*(dt_3/2), 0,
+	     0, noise_ay_*(dt_4/4), 0, noise_ay_*(dt_3/2),
+             noise_ax_*(dt_3/2), 0, noise_ax_*dt_2, 0,
+             0, noise_ay_*(dt_3/2), 0, noise_ay_*dt_2;
+  
+  ekf_.Predict();
+*/
+  /*****************************************************************************
+   *  Update
+   ****************************************************************************/
+
+  /**
+     * Use the sensor type to perform the update step.
+     * Update the state and covariance matrices.
+   */
+/*
+  if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
+    // Radar updates
+    ekf_.H_ = tools.CalculateJacobian(ekf_.x_); // KRO set to Hj which should be set using the Jacobian function in tools.cpp
+    ekf_.R_ = R_radar_;
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);  //KRO
+  } else {
+    // Laser updates
+    ekf_.H_ = H_laser_;
+    ekf_.R_ = R_laser_;
+    ekf_.Update(measurement_pack.raw_measurements_);  //KRO
+  }
+*/
+  // print the output
+  //cout << "x_ = " << ekf_.x_ << endl;
+  //cout << "P_ = " << ekf_.P_ << endl;
 }
 
 /**
