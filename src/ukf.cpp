51a3674453c92688b31201dbeaaffe4e62d2b5a9
Ken Overholt <kroverholt@gmail.com>
2017-09-03 21:45:59 -0400

    Add Sig point prediction

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -72,6 +72,9 @@ UKF::UKF() {
   //create sigma point matrix
   Xsig_aug_ = MatrixXd(n_aug_, 2 * n_aug_ + 1);	//KRO2 added
 
+  //create matrix with predicted sigma points as columns
+  Xsig_pred_ = MatrixXd(n_x_, 2 * n_aug_ + 1);
+
 }
 
 UKF::~UKF() {}
@@ -208,7 +211,7 @@ void UKF::Prediction(double delta_t) {
   */
   
   ////////////////////////////////////////////////////////////////////////
-  // Create Augmented Signa Points
+  // Create Augmented Signa Points	//L7, sect 18
   ///////////////////////////////////////////////////////////////////////
   
   //create augmented mean state
@@ -233,7 +236,58 @@ void UKF::Prediction(double delta_t) {
     Xsig_aug_.col(i+1+n_aug_) = x_aug_ - sqrt(lambda_+n_aug_) * L.col(i);
   }
 
-}
+  ////////////////////////////////////////////////////////////////////////////////////////
+  // Predict Sigma Points   L7, sect 21
+  ///////////////////////////////////////////////////////////////////////////////
+
+    //predict sigma points
+  for (int i = 0; i< 2*n_aug_+1; i++)
+  {
+    //extract values for better readability
+    double p_x = Xsig_aug_(0,i);
+    double p_y = Xsig_aug_(1,i);
+    double v = Xsig_aug_(2,i);
+    double yaw = Xsig_aug_(3,i);
+    double yawd = Xsig_aug_(4,i);
+    double nu_a = Xsig_aug_(5,i);
+    double nu_yawdd = Xsig_aug_(6,i);
+
+    //predicted state values
+    double px_p, py_p;
+
+    //avoid division by zero
+    if (fabs(yawd) > 0.001) {
+        px_p = p_x + v/yawd * ( sin (yaw + yawd*delta_t) - sin(yaw));
+        py_p = p_y + v/yawd * ( cos(yaw) - cos(yaw+yawd*delta_t) );
+    }
+    else {
+        px_p = p_x + v*delta_t*cos(yaw);
+        py_p = p_y + v*delta_t*sin(yaw);
+    }
+
+    double v_p = v;
+    double yaw_p = yaw + yawd*delta_t;
+    double yawd_p = yawd;
+
+    //add noise
+    px_p = px_p + 0.5*nu_a*delta_t*delta_t * cos(yaw);
+    py_p = py_p + 0.5*nu_a*delta_t*delta_t * sin(yaw);
+    v_p = v_p + nu_a*delta_t;
+
+    yaw_p = yaw_p + 0.5*nu_yawdd*delta_t*delta_t;
+    yawd_p = yawd_p + nu_yawdd*delta_t;
+
+    //write predicted sigma point into right column
+    Xsig_pred(0,i) = px_p;
+    Xsig_pred(1,i) = py_p;
+    Xsig_pred(2,i) = v_p;
+    Xsig_pred(3,i) = yaw_p;
+    Xsig_pred(4,i) = yawd_p;
+  }  //end predict sigma points    for (int i = 0; i< 2*n_aug+1; i++)
+
+	
+
+}  //end void UKF::Prediction(double delta_t) {
 
 /**
  * Updates the state and the state covariance matrix using a laser measurement.
