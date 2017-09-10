a2641ee5690d659daa4c155f5ec26d015132ffd3
Ken Overholt <kroverholt@gmail.com>
2017-09-10 17:29:14 -0400

    set z_ for radar

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -37,7 +37,8 @@ UKF::UKF() {
   // Process noise standard deviation yaw acceleration in rad/s^2
   std_yawdd_ = 0.2;	//KRO2: will need to modify this (original value: 30)
 
-  // Laser measurement noise standard deviation position1 in m
+  // Laser 
+	noise standard deviation position1 in m
   std_laspx_ = 0.15;
 
   // Laser measurement noise standard deviation position2 in m
