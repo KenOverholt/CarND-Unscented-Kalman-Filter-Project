6964d7f176967aa67b303be497a88a521e002736
Ken Overholt <kroverholt@gmail.com>
2017-09-10 22:27:58 -0400

    capitalize laser

--- src/ukf.h
+++ src/ukf.h
@@ -90,8 +90,8 @@ public:
   //declare vector for z, incoming radar measurement
   VectorXd z_;  //KRO2 added
 
-  MatrixXd H_laser_;  //KRO2 added
-  MatrixXd R_laser_;  //KRO2 added
+  MatrixXd H_Laser_;  //KRO2 added
+  MatrixXd R_Laser_;  //KRO2 added
   
   /**
    * Constructor
