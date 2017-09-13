7ede1c0f659698d4e7aec079849ba1b34434e6ac
Ken Overholt <kroverholt@gmail.com>
2017-09-13 18:14:10 -0400

    Update ukf.cpp

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -17,10 +17,10 @@ UKF::UKF() {
   previous_timestamp_ = 0;
   
   // if this is false, laser measurements will be ignored (except during init)
-  use_laser_ = true;
+  use_laser_ = false;
 
   // if this is false, radar measurements will be ignored (except during init)
-  use_radar_ = false;
+  use_radar_ = true;
 
   //set n_x_
   n_x_ = 5;  //KRO2 added
