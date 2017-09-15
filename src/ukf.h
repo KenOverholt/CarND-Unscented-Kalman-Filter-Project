aa51a45f9b73972b002700d0f08e75b8f9876e52
Ken Overholt <kroverholt@gmail.com>
2017-09-14 20:34:28 -0400

    Update ukf.h

--- src/ukf.h
+++ src/ukf.h
@@ -87,9 +87,6 @@ public:
   //matrix for cross correlation Tc_
   MatrixXd Tc_;  //KRO2 added
 
-  //declare vector for z, incoming radar measurement
-  VectorXd z_;  //KRO2 added
-
   MatrixXd H_Laser_;  //KRO2 added
   MatrixXd R_Laser_;  //KRO2 added
   
